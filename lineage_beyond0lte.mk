0a8481ee2a0a9d221970ddf37814da0ad1eaebff
Andreas Schneider <asn@cryptomilk.org>
2020-02-27 20:04:32 +0100

    lineage: This launched with P

--- lineage_beyond0lte.mk
+++ lineage_beyond0lte.mk
@@ -6,7 +6,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/samsung/beyond0lte/device.mk)
 
 # Product API level
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 ### BOOTANIMATION
 # vendor/lineage/config/common_full_phone.mk
