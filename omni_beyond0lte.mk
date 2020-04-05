064c57d87f8e765b3cb78552384ef82af493f5fd
Andreas Schneider <asn@cryptomilk.org>
2020-04-05 06:50:20 +0200

    omni: Fix product definitions

--- omni_beyond0lte.mk
+++ omni_beyond0lte.mk
@@ -16,12 +16,13 @@ $(call inherit-product, vendor/omni/config/common.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_beyond0lte
 PRODUCT_DEVICE := beyond0lte
-PRODUCT_BRAND := Samsung
+PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G970F
-PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=beyond0lte \
+    PRODUCT_NAME=beyond0ltexx \
+    PRODUCT_DEVICE=beyond0 \
     PRIVATE_BUILD_DESC="beyond0ltexx-user 10 QP1A.190711.020 G970FXXU4BTA8 release-keys"
 
 BUILD_FINGERPRINT := samsung/beyond0ltexx/beyond0:10/QP1A.190711.020/G970FXXU4BTA8:user/release-keys
