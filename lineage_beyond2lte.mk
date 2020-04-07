6355c51a506bed954587dea044cc72dac3c68546
Zunaid Amin Enan <zunaid321@gmail.com>
2020-04-07 15:42:13 +0600

    Renamed to lineage_beyond2lte.mk and updated with beyond2lte values

--- lineage_beyond2lte.mk
+++ lineage_beyond2lte.mk
@@ -5,12 +5,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/beyond0lte/device.mk)
+$(call inherit-product, device/samsung/beyond2lte/device.mk)
 
 ### BOOTANIMATION
 # vendor/lineage/config/common_full_phone.mk
-TARGET_SCREEN_HEIGHT := 2280
-TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 3040
+TARGET_SCREEN_WIDTH := 1440
 # vendor/lineage/config/common.mk
 TARGET_BOOTANIMATION_HALF_RES := true
 
@@ -18,20 +18,20 @@ TARGET_BOOTANIMATION_HALF_RES := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_beyond0lte
-PRODUCT_DEVICE := beyond0lte
+PRODUCT_NAME := lineage_beyond2lte
+PRODUCT_DEVICE := beyond2lte
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-G970F
+PRODUCT_MODEL := SM-G975F
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_PDA_VERSION := G970FXXU4CTC9
+PRODUCT_PDA_VERSION := G975FXXU4CTC9
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=beyond0ltexx \
-    PRODUCT_DEVICE=beyond0 \
-    PRIVATE_BUILD_DESC="beyond0ltexx-user 10 QP1A.190711.020 $(PRODUCT_PDA_VERSION) release-keys"
+    PRODUCT_NAME=beyond2ltexx \
+    PRODUCT_DEVICE=beyond2 \
+    PRIVATE_BUILD_DESC="beyond2ltexx-user 10 QP1A.190711.020 $(PRODUCT_PDA_VERSION) release-keys"
 
-BUILD_FINGERPRINT := samsung/beyond0ltexx/beyond0:10/QP1A.190711.020/$(PRODUCT_PDA_VERSION):user/release-keys
+BUILD_FINGERPRINT := samsung/beyond2ltexx/beyond2:10/QP1A.190711.020/$(PRODUCT_PDA_VERSION):user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.PDA=$(PRODUCT_PDA_VERSION)
